c6d4af2180723a28fd7008d6cc08fc45023de2d8
Makkovs <kovalmax09@gmail.com>
2021-02-02 21:25:38 +0200

    Index Interface Update

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ua">
 <head>
  <meta charset="UTF-8">
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,20 +7,20 @@
  <link rel="stylesheet" href="./css/index.css">
 </head>
 <body>
-<div class="menu">
-      <div class="container">
-        <h1 id = "h">INTEX</h1>
-        <div class="links">
-        <a id = "link1" href ="index.php">Про нас</a>
-        <a id = "link2" href ="service.php">Послуги</a>
-        <a id = "link3" href ="shop.php">Товари</a>
-       </div>
-       <img src="../images/viber.svg" width="70" height="70" id="viber">
-       <div class="number1"><h1>+380930395434</h1></div>
-       <img src="../images/phone.svg" width="70" height="70" id="phone">
-       <div class="number2"><h1>+380930395434</h1></div>
-       <img src="../images/gmail.svg" width="70" height="70" id="gmail">
-       <div class="number3"><h1>0395434@gmail.com</h1></div>   
-      </div>
-     </div>
+  <header>
+    <h1 class = "h">INTEX</h1>
+    <nav>
+      <a class = "link1" href ="index.php">Про нас</a>
+      <a class = "link2" href ="service.php">Послуги</a>
+      <a class = "link3" href ="shop.php">Товари</a>
+    </nav>
+  </header>
+  <div class="content">
+    <img src="../images/viber.svg" width="70" height="70">
+    <span class="number">+380930395434<span> <br>
+    <img src="../images/phone.svg" width="70" height="70">
+    <span class="number">+380930395434<span> <br>
+    <img src="../images/gmail.svg" width="70" height="70">
+    <span class="number">0395434@gmail.com<span>  
+  </div>
 </body>
--- service.php
+++ service.php
@@ -7,46 +7,15 @@
  <link rel="stylesheet" href="./css/service.css">
 </head>
 <body>
-     <div class="menu">
-      <div class="container">
-        <h1 id = "h">INTEX</h1>
-        <div class="links">
-        <a id = "link1" href ="index.php">Про нас</a>
-        <a id = "link2" href ="service.php">Послуги</a>
-        <a id = "link3" href ="shop.php">Товари</a>
-        </div>
-        <h1 id="hh">Друк</h1>
-        <table border="0" id="tt">
-        <tr>
-            <th>Розмір:</th>
-            <th>Чорно-білий:</th>
-            <th>Кольоровий:</th>
-       </tr>
-       <tr><td>А4</td><td>-</td><td>-</td></tr>
-       <tr><td>А3</td><td>-</td><td>-</td></tr>       
-        </table>
-        <h1 id="hhh">Фотодрук</h1>
-        <table border="0" id="ttt">
-            <tr>
-                <th>Розмір:</th>
-                <th>Вартість:</th>
-            </tr>
-            <tr><td>10х15</td><td>-</td></tr>
-            <tr><td>13х18</td><td>-</td></tr>
-            <tr><td>15х21</td><td>-</td></tr>
-            <tr><td>21х30</td><td>-</td></tr>
-        </table>
-        <h1 id="hhhh">Інші послуги</h1>
-        <table border="0" id="tttt">
-            <tr>
-                <th>Послуга:</th>
-                <th>Вартість:</th>
-                <th>Послуга:</th>
-                <th>Вартість:</th>
-            </tr>
-            <tr><td>Ксерокопія:</td><td>-</td><td>Сканування:</td><td>-</td></tr>
-            <tr><td>Ламінування:</td><td>-</td><td>Брошурування:</td><td>-</td></tr>
-        </table>
+    <header>
+        <h1 class = "h">INTEX</h1>
+        <nav>
+            <a class = "link1" href ="index.php">Про нас</a>
+            <a class = "link2" href ="service.php">Послуги</a>
+            <a class = "link3" href ="shop.php">Товари</a>
+        </nav>
+    </header>
+    <div class="content">
+
     </div>
-     </div>
 </body>
